14d066ed506aeba6f2903d623daec1dcaed85e43
Mike Fuchsman <mike.fuchsman@summitmediadesign.com>
2017-11-26 23:44:10 -0700

    First

--- server.js
+++ server.js
@@ -1,58 +1,76 @@
+
+// Dependencies and Scraping Tools
+const request = require('request');
 const cheerio = require('cheerio');
 const express = require('express');
 const mongojs = require('mongojs');
-const request = require('request');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const path = require('path');
 
+// Required JS file dependencies
+var Note = require('./models/note.js');
+var Article = require('./models/article.js');
+
+// Initialize Express
 const app = express();
 const db = mongojs('scraper', ['webdevdata']);
 
+// Establish using BodyParser in app
+app.use(bodyParser.urlencoded({ extended: false }));
 
+// Set Public as a static directory
+app.use(express.static('public'));
 
-// Remove all the documents from the collection
-// THIS IS BAD. DON'T DO THIS IN PRODUCTION!
-// This is causing the app to reload its entire dataset every time
-// the app is run. That's really bad. Don't do this.
-db.webdevdata.remove({});
+// Set handlebars
+const exphbs = require('express-handlebars')
 
-// Grab data from reddit
-request('https://www.reddit.com/r/webdev', (error, response, html) => {
-    // Initialize $ variable with the page's DOM
-    var $ = cheerio.load(html);
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main',
+}));
 
-    // Iterate over each 'div.top-matter' element (each post on the page)
-    $('div.top-matter').each(function(i, element) {
-        // Grab title
-        var title = $(element).find('a.title').text();
+app.set('view engine', 'handlebars');
 
-        // Grab comment count
-        var comments = $(element).find('a.comments').text();
-        // This regex matches the number "5" in "5 comments"
-        var match = comments.match(/(\d+) comment/);
-        comments = match ? match[1] : 0;
+// GET data from the New York Times
+  request('https://www.nytimes.com/',
+  function(error, response, html) {
+// Initialize $ variable with the page's DOM
+  var $ = cheerio.load(html);
+// Iterate over each 'div.top-matter' element (each post on the page)
+  $('div.top-matter').each(function(i, element) {
 
-        // Grab author
-        var author = $(element).find('a.author').text();
+    // Grab title
+    var title = $(element).find('a.title').text();
 
-        // Grab timestamps
-        var timestampHuman = $(element).find('time.live-timestamp').text();
-        var timestampMachine = $(element).find('time.live-timestamp').attr('datetime');
+    // Grab comment count
+    var comments = $(element).find('a.comments').text();
+    // This regex matches the number "5" in "5 comments"
+    var match = comments.match(/(\d+) comment/);
+    comments = match ? match[1] : 0;
 
-        // Create data object
-        var post = {
-            title: title,
-            author: author,
-            commentCount: comments,
-            timestamp: timestampMachine,
-            timestampDisplay: timestampHuman
-        };
-        console.log(JSON.stringify(post, null, 2));
+    // Grab author
+    var author = $(element).find('a.author').text();
 
-        // Insert into database
-        db.webdevdata.insert(post);
-        console.log('record inserted!');
-    });
+    // Grab timestamps
+    var timestampHuman = $(element).find('time.live-timestamp').text();
+    var timestampMachine = $(element).find('time.live-timestamp').attr('datetime');
 
-    //db.close();
+    // Create data object
+    var post = {
+        title: title,
+        author: author,
+        commentCount: comments,
+        timestamp: timestampMachine,
+        timestampDisplay: timestampHuman
+    };
+    console.log(JSON.stringify(post, null, 2));
+
+    // Insert into database
+    db.webdevdata.insert(post);
+    console.log('record inserted!');
+});
+
+//db.close();
 });
 
 // Data route
