76809afb0d091e919ea1ee775a4c5a289b243706
Mike Fuchsman <mike.fuchsman@summitmediadesign.com>
2017-12-03 23:13:00 -0700

    pull not saving

---
+++ models/article.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+    var ArticleSchema = new Schema ({
+        headline: String,
+        link: String,
+        summary: String,
+    });
+
+var Article = mongoose.model('Article', ArticleSchema);
+
+module.exports = Article;
--- server.js
+++ server.js
@@ -1,74 +1,108 @@
-
 // Dependencies and Scraping Tools
-var request = require('request');
+var bodyParser = require('body-parser');
 var cheerio = require('cheerio');
 var express = require('express');
+var logger = require("morgan");
 var mongojs = require('mongojs');
 var mongoose = require('mongoose');
-var bodyParser = require('body-parser');
 var path = require('path');
+var request = require('request');
 
-// // Required JS file dependencies
-// var Note = require('./models/note.js');
-// var Article = require('./models/article.js');
+var Note = require('./models/note.js');
+var Article = require('./models/article.js');
 
-// Initialize Express
+mongoose.Promise = Promise;
+
+// Establish express and body-parser
 var app = express();
+
+// Use Morgan and Body-Parser
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Set Public as a static directory
+app.use(express.static('public'));
+
+// Set handlebars
+var exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 // Database configuration
 var databaseURL = 'scraper';
 var collections = ['scrapedData'];
 
-
 // Connect mongojs configuration to the constructor
-var db = mongojs(databaseURL, collections);
+mongoose.connect("mongodb://localhost/scrape")
+// var db = mongojs(databaseURL, collections);
+const db = mongoose.connection;
 db.on('error', function(error){
-  console.log('Database error: ', error);
+  console.log("Database error: ", error);
 });
 
-// Test route to make sure its up and working properly
-
-app.get('/', function(req, res){
-  res.send('Hello world');
+// Is the db connection successful?
+db.once('open', function(){
+  console.log('Mongoose connection is a go!');
 });
 
-app.get('/all', function(req, res){
-  db.scrapedData.find({}, function(error, found){
-    if(error){
-      console.log(error);
-    } else {
-      {
-        res.json(found);
-      }
-    }
-  });
+// The Routes
+app.get('/', (req, res) => {
+	res.redirect('/all');
 });
 
 app.get('/scrape', function(req, res){
-  // GET data from the New York Times
-    request('https://www.washingtonpost.com/', function(error, response, html){
-      var $ = cheerio.load(html);
+  Article.remove(function(err,removed) {
+		   // where removed is the count of removed documents
+		});
+  // GET data from the Washington Post Sports Section
+    request('https://www.washingtonpost.com/sports/', function(error, response, body){
+      var $ = cheerio.load(body);
 
-      $('.headline').each(function(i, element){
-        var headline = $(this).children('a').text();
-        var link = $(this).children('a').attr('href');
+      $('.story-body').each(function(i, element){
+        var headline = $(element).find('h3').text().trim();
+        var link = $(element).find('h3').children('a').attr('href');
+        var summary = $(element).find('.story-description').children('p').text().trim();
 
-          if (headline && link){
-            db.scrapedData.save({
-              title: headline,
-              link: link
-            },
-            function(error, saved){
-              if(error){
-                console.log(error);
-              } else {
-                console.log(saved);
-              }
-          });
-          }
-      });
+
+        // Data object
+        var post = {
+            headline: headline,
+            link: link,
+            summary: summary
+        };
+        console.log(JSON.stringify(post, null, 2));
+
+        // Insert into database
+        Article.create(post)
+        .then(function(dbArt) {
+          res.redirect('/all');
+        });
+        console.log('record inserted!');
+
+
+
+			})
+
     });
-    res.send('Scrape is complete.');
 });
+
+app.get('/all', function(req, res) {
+	Article
+	.find({})
+    .then(function(dbArt) {
+
+        var hbsObject = {
+			articles: dbArt
+        };
+
+        res.render('index', hbsObject);
+    })
+    .catch(function(err) {
+
+		res.json(err);
+    });
+});
+
 // Start express server
-app.listen(8080, () => console.log('Example app listening on port 8080!'));
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
