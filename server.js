c87599a656adf67035bb608e3156368dbf991123
Mike Fuchsman <mike.fuchsman@summitmediadesign.com>
2017-12-06 19:44:21 -0700

    new mongo

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.set('view engine', 'handlebars');
 // var collections = ['scrapedData'];
 
 // Connect mongojs configuration to the constructor
-mongoose.connect("mongodb://mfoosh:mfoosh@ds129796.mlab.com:29796/heroku_pvd5mcwh/db")
+mongoose.connect("mongodb://<dbuser>:<dbpassword>@ds111771.mlab.com:11771/heroku_fgqt2k6t")
 // mongoose.connect("mongodb://localhost/scrape/")
 // var db = mongojs(databaseURL, collections);
 //
